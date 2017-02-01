1bc28db9cce68dcbda975a9368b5a0db47a76efc
Fukazu Tomoaki <t.fukazu@gmail.com>
2017-02-02 05:42:22 +0900

    list bug fix

--- sorter_fusion.js
+++ sorter_fusion.js
@@ -1,3 +1,10 @@
 $(function() {
   $("#fusionTable").tablesorter();
+  $("#fusionTable0").tablesorter();
+  $("#fusionTable1").tablesorter();
+  $("#fusionTable2").tablesorter();
+  $("#fusionTable3").tablesorter();
+  $("#fusionTable4").tablesorter();
+  $("#fusionTable5").tablesorter();
+  $("#fusionTable6").tablesorter();  
 });
